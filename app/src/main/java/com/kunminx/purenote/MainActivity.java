e281a67c7f96aa0c5d12fdcfc6b3ee97c8b0180c
kunminx <kunminx@gmail.com>
2022-06-30 19:26:23 +0800

    first commit

---
+++ app/src/main/java/com/kunminx/purenote/MainActivity.java
@@ -0,0 +1,17 @@
+package com.kunminx.purenote;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+
+  }
+
+
+}
