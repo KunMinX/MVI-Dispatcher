4b07f181b70f35f911992fc36b7623e4b37ea18b
kunminx <kunminx@gmail.com>
2022-06-30 23:30:13 +0800

    add list and editor page

--- app/src/main/java/com/kunminx/purenote/ui/page/EditorFragment.java
+++ app/src/main/java/com/kunminx/purenote/ui/page/EditorFragment.java
@@ -1,20 +1,30 @@
 package com.kunminx.purenote.ui.page;
 
+import androidx.lifecycle.ViewModel;
+
 import com.kunminx.architecture.ui.page.BaseFragment;
 import com.kunminx.architecture.ui.page.DataBindingConfig;
+import com.kunminx.purenote.BR;
+import com.kunminx.purenote.R;
 
 /**
  * Create by KunMinX at 2022/6/30
  */
 public class EditorFragment extends BaseFragment {
 
+  private EditorViewModel mStates;
+
   @Override
   protected void initViewModel() {
-
+    mStates = getFragmentScopeViewModel(EditorViewModel.class);
   }
 
   @Override
   protected DataBindingConfig getDataBindingConfig() {
-    return null;
+    return new DataBindingConfig(R.layout.fragment_editor, BR.vm, mStates);
+  }
+
+  public static class EditorViewModel extends ViewModel {
+
   }
 }
--- app/src/main/java/com/kunminx/purenote/ui/page/ListFragment.java
+++ app/src/main/java/com/kunminx/purenote/ui/page/ListFragment.java
@@ -1,20 +1,30 @@
 package com.kunminx.purenote.ui.page;
 
+import androidx.lifecycle.ViewModel;
+
 import com.kunminx.architecture.ui.page.BaseFragment;
 import com.kunminx.architecture.ui.page.DataBindingConfig;
+import com.kunminx.purenote.BR;
+import com.kunminx.purenote.R;
 
 /**
  * Create by KunMinX at 2022/6/30
  */
 public class ListFragment extends BaseFragment {
 
+  private ListViewModel mStates;
+
   @Override
   protected void initViewModel() {
-
+    mStates = getFragmentScopeViewModel(ListViewModel.class);
   }
 
   @Override
   protected DataBindingConfig getDataBindingConfig() {
-    return null;
+    return new DataBindingConfig(R.layout.fragment_list, BR.vm, mStates);
+  }
+
+  public static class ListViewModel extends ViewModel {
+
   }
 }
