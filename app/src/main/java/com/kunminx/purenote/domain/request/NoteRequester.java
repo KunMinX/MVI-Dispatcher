450ad8c1cdd063a36cf1d594c370815af5a17f0e
kunminx <kunminx@gmail.com>
2022-07-03 14:40:26 +0800

    remote origin request

--- app/src/main/java/com/kunminx/purenote/domain/request/NoteRequester.java
+++ app/src/main/java/com/kunminx/purenote/domain/request/NoteRequester.java
@@ -1,59 +1,53 @@
 package com.kunminx.purenote.domain.request;
 
-import androidx.lifecycle.ViewModel;
-
-import com.kunminx.architecture.domain.message.MutableResult;
-import com.kunminx.architecture.domain.message.Result;
+import com.kunminx.architecture.domain.dispatch.TruthDispatcher;
 import com.kunminx.purenote.data.repo.DataRepository;
 import com.kunminx.purenote.domain.event.NoteListEvent;
 
 /**
  * Create by KunMinX at 2022/6/14
  */
-public class NoteRequester extends ViewModel {
+public class NoteRequester extends TruthDispatcher<NoteListEvent> {
 
-  private final MutableResult<NoteListEvent> noteListResult = new MutableResult<>();
+  @Override
+  public void input(NoteListEvent event) {
+    super.input(event);
 
-  public Result<NoteListEvent> getNoteListResult() {
-    return noteListResult;
-  }
-
-  public void requestNoteList(NoteListEvent noteListEvent) {
-    switch (noteListEvent.eventId) {
+    switch (event.eventId) {
       case NoteListEvent.EVENT_GET_NOTE_LIST:
         DataRepository.getInstance().getNotes(dataResult -> {
-          noteListEvent.result.notes = dataResult.getResult();
-          noteListResult.setValue(noteListEvent);
+          event.result.notes = dataResult.getResult();
+          getResult(event.eventId).setValue(event);
         });
         break;
       case NoteListEvent.EVENT_UPDATE_ITEM:
       case NoteListEvent.EVENT_MARK_ITEM:
-        DataRepository.getInstance().updateNote(noteListEvent.param.note, dataResult -> {
-          noteListEvent.result.isSuccess = dataResult.getResult();
-          noteListResult.setValue(noteListEvent);
+        DataRepository.getInstance().updateNote(event.param.note, dataResult -> {
+          event.result.isSuccess = dataResult.getResult();
+          getResult(event.eventId).setValue(event);
         });
         break;
       case NoteListEvent.EVENT_TOPPING_ITEM:
-        DataRepository.getInstance().updateNote(noteListEvent.param.note, dataResult -> {
-          noteListEvent.result.isSuccess = dataResult.getResult();
-          if (noteListEvent.result.isSuccess) {
+        DataRepository.getInstance().updateNote(event.param.note, dataResult -> {
+          event.result.isSuccess = dataResult.getResult();
+          if (event.result.isSuccess) {
             DataRepository.getInstance().getNotes(dataResult1 -> {
-              noteListEvent.result.notes = dataResult1.getResult();
-              noteListResult.setValue(noteListEvent);
+              event.result.notes = dataResult1.getResult();
+              getResult(event.eventId).setValue(event);
             });
           }
         });
         break;
       case NoteListEvent.EVENT_ADD_ITEM:
-        DataRepository.getInstance().insertNote(noteListEvent.param.note, dataResult -> {
-          noteListEvent.result.isSuccess = dataResult.getResult();
-          noteListResult.setValue(noteListEvent);
+        DataRepository.getInstance().insertNote(event.param.note, dataResult -> {
+          event.result.isSuccess = dataResult.getResult();
+          getResult(event.eventId).setValue(event);
         });
         break;
       case NoteListEvent.EVENT_REMOVE_ITEM:
-        DataRepository.getInstance().deleteNote(noteListEvent.param.note, dataResult -> {
-          noteListEvent.result.isSuccess = dataResult.getResult();
-          noteListResult.setValue(noteListEvent);
+        DataRepository.getInstance().deleteNote(event.param.note, dataResult -> {
+          event.result.isSuccess = dataResult.getResult();
+          getResult(event.eventId).setValue(event);
         });
         break;
     }
