048473c84324a373c447f32328c98c7c47c8d964
kunminx <kunminx@gmail.com>
2022-08-15 16:46:19 +0800

    add setting page

---
+++ app/src/main/java/com/kunminx/purenote/ui/page/SettingFragment.java
@@ -0,0 +1,26 @@
+package com.kunminx.purenote.ui.page;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.kunminx.architecture.ui.page.BaseFragment;
+
+/**
+ * Create by KunMinX at 2022/8/15
+ */
+public class SettingFragment extends BaseFragment {
+
+  @Override
+  protected void onInitViewModel() {
+
+  }
+
+  @Override
+  protected View onInitView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+    return null;
+  }
+}
